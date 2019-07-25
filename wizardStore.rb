05aadcc8ea868df38e043ea1954100a371c267a2
EdwardGoomba <edward@dock90.io>
2019-07-24 20:08:20 -0400

    Setup welcome to wizard store

---
+++ wizardStore.rb
@@ -0,0 +1,15 @@
+
+# display welcome message
+def print_welcome
+  # define welcome message
+  welcome_message = 'Welcome to Edwards Enchacements!'
+  # define tagline
+  tagline = 'We sell all sorts of magical spells and potions for Aurors'
+  puts welcome_message
+  sleep(2)
+  puts tagline
+  sleep(1)
+  puts 'What are you looking for today?'
+end
+
+print_welcome
