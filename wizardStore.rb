6315c0f004a56ecf819aa56e97f548679eac5a61
EdwardGoomba <edward@dock90.io>
2019-07-24 20:32:53 -0400

    Ask traveler what they are looking for

--- wizardStore.rb
+++ wizardStore.rb
@@ -6,10 +6,17 @@ def print_welcome
   # define tagline
   tagline = 'We sell all sorts of magical spells and potions for Aurors'
   puts welcome_message
-  sleep(2)
+  sleep 2
   puts tagline
-  sleep(1)
+  sleep 1
   puts 'What are you looking for today?'
 end
 
+def needs
+  sleep 1
+  need = gets
+  puts 'Oh lovely! Good thing we have ' + need + ' in stock!'
+end
+
 print_welcome
+needs
