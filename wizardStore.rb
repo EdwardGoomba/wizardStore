483a1dd32d6b0619e9b356b7ac8579c1dfd8bdcc
EdwardGoomba <edward@dock90.io>
2019-09-20 13:51:24 -0400

    List spells and check for spell types

--- wizardStore.rb
+++ wizardStore.rb
@@ -9,13 +9,22 @@ def print_welcome
   sleep 2
   puts tagline
   sleep 1
+  puts 'We have chompers, whizbangs, lindops and a variety of other surprises.'
   puts 'What are you looking for today?'
 end
 
 def needs
   sleep 1
   need = gets.chomp
-  puts 'Oh lovely! Good thing we have ' + need + ' in stock!'
+  if (need == 'chompers')
+    puts "Oh lovely! We just got a fresh set of #{need} this morning!"
+  elsif need == 'whizbangs'
+    puts "Great scott! What are you planning to do with #{need}?"
+  elsif need == 'lindops'
+    puts "Oh my. I remember when my truelove got me my first batch of #{need}.."
+  else
+    puts "Hmm.. we dont seem to have #{need} in stock today. Sorry!"
+  end
 end
 
 print_welcome
